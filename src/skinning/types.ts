4d89f031de41f218b863ee619669f00f0a9640b4
Laird Kruger <accounts@lairdkruger.com>
2026-04-26 14:01:42 +1000

    Skinning cleanup

--- src/skinning/types.ts
+++ src/skinning/types.ts
@@ -1,14 +1,8 @@
 import { Mat4 } from "../math";
 
-export class SkinData {
-  public joints: { transform: { worldMatrix: Mat4 } }[];
-  public inverseBindMatrices: Mat4[];
+type Joint = { transform: { worldMatrix: Mat4 } };
 
-  constructor(
-    joints: { transform: { worldMatrix: Mat4 } }[],
-    inverseBindMatrices: Mat4[],
-  ) {
-    this.joints = joints;
-    this.inverseBindMatrices = inverseBindMatrices;
-  }
-}
+export type SkinData = {
+  joints: Joint[];
+  inverseBindMatrices: Mat4[];
+};
